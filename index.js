b7fa89a277feeff8509c56560855512d0aff46ab
mnlivvv <tranminhnhan271099@gmail.com>
2020-04-16 22:03:55 +0700

    pug template

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ var app = express();
 
 var port = 3000;
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.get('/', function(request, response){
-	response.send('Hello coder')	
-})
+	response.render('index', {
+		name: 'AAA'
+	})	
+});
+
+app.get('/users', function(req, res){
+	res.render('users/index', {
+		users: [
+			{ id: 1, name: 'Nhan' },
+			{ id: 2, name: 'Lien'}
+		]
+	});
+});
 
 app.listen(port, function(){
 	console.log('Server listening on port ' + port)
