d2e7b0c3690b5c18fbf9cc4eab19703b181aa06e
mnlivvv <tranminhnhan271099@gmail.com>
2020-04-16 22:33:40 +0700

    query para

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+var users = [
+			{ id: 1, name: 'Nhan' },
+			{ id: 2, name: 'Lien'}
+		]
+
 app.get('/', function(request, response){
 	response.render('index', {
 		name: 'AAA'
@@ -14,12 +19,21 @@ app.get('/', function(request, response){
 
 app.get('/users', function(req, res){
 	res.render('users/index', {
-		users: [
-			{ id: 1, name: 'Nhan' },
-			{ id: 2, name: 'Lien'}
-		]
+		users: users
 	});
 });
+
+app.get('/users/search', function(req, res){
+	var q = req.query.q;
+	var matchedUsers = users.filter(function(user){
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	})
+
+	res.render('users/index', {
+		users: matchedUsers,
+		currentSearch: q
+	})
+})
 
 app.listen(port, function(){
 	console.log('Server listening on port ' + port)
